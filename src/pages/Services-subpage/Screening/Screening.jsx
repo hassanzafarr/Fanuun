b826b8030c6c67208935ff1457e3b6db011cb6ad
hassanzafarr <hassanzafar619@gmail.com>
2023-03-08 12:38:54 +0500

    changes

--- src/pages/Services-subpage/Screening/Screening.jsx
+++ src/pages/Services-subpage/Screening/Screening.jsx
@@ -5,42 +5,12 @@ import { Footer } from "../../../containers";
 import Banner from "./../../../components/banner/Banner";
 import image from "../../../assets/screening.png";
 
-import Contfan from "../../../components/contact-fannuun/contfan";
-import { Info } from "./../../../components/info/Info";
-
 const Screening = () => {
-  const data = [
-    "ID Card Verification",
-    "Verification of Employment Data",
-    "Verification of Educational Documents",
-    "Verification of Address",
-    "Verification of Vehicle Details",
-    "Verification of Driver's License",
-    "Marriage Certificate Verification",
-    "Divorce Certificate Verification",
-    "Police Verification",
-    "Newspaper Notice Checks",
-    "Civil Litigation Checks",
-    "Reference Checks",
-    "Verification of Vendor's Business",
-  ];
-
   return (
     <div className="screening__main-container">
       <Navbar />
       <div className="section__container section__padding">
         <Banner text="Screening Services" img={image} />
-        <div className="info__container">
-          <div className="info__container-feature">
-            <Info
-              title="Fanuun offers reliable and thorough background screening services to individuals and firms.
-The data/documents screened under this include:"
-              text={data}
-              isList={true}
-            />
-          </div>
-        </div>
-        <Contfan />
       </div>
       <Footer />
     </div>
--- src/pages/UK/UK.jsx
+++ src/pages/UK/UK.jsx
@@ -2,15 +2,25 @@ import React from "react";
 
 import "./UK.css";
 import shape5 from "../../assets/Group 47.png";
-
+import back from "../../assets/backimage.svg";
 import { Brand, Navbar } from "../../components";
 import { Footer } from "../../containers";
 import Fade from "react-reveal/Fade";
+import freedom from "../../assets/freedom.svg";
+import global from "../../assets/global.svg";
+import home from "../../assets/mhome.svg";
+import fast from "../../assets/fast.svg";
+import lower from "../../assets/lower.svg";
+import flexible from "../../assets/flexible.svg";
+import startup from "../../assets/Startup.svg";
+import innovate from "../../assets/Innovator.svg";
+import right from "../../assets/rightwavy.svg";
+import { CTA } from "./../../components";
 
 const UK = () => (
   <div className="uk__main-container">
     <Navbar />
-    <div className="uk__possiblity-main ">
+    <div className="uk__possiblity-main  ">
       <div className="uk__possibility section__padding " id="possibility">
         <div className="uk__possibility-content">
           <Fade top>
@@ -27,28 +37,88 @@ const UK = () => (
         </div>
       </div>
 
-      {/* <div className="uk__section-ability ">
+      <div className="uk__section-ability  ">
         <div className="uk__section-ability-content section__padding">
           <Fade top>
             <h1 style={{ color: "#060056" }}>
-              Eligibility for Canada Start-Up Visa
+              Eligibility for uk Start-Up Visa
             </h1>
             <p>Applying entrepreneurs must,</p>
           </Fade>
         </div>
+      </div>
+      <div
+        className="uk__section-help section__padding "
+        style={{
+          backgroundImage: `url(${back})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="uk__section-logo-container">
+          <div className="uk-container-a">
+            <div className="uk-img-cont">
+              <img src={freedom} alt="prof" />
+            </div>
+            <p>
+              Have a business that qualifies specified criteria by the Canadian
+              government.
+            </p>
+          </div>
+          <div className="uk-container-a">
+            <div className="uk-img-cont">
+              <img src={global} alt="prof" />
+            </div>
+            <p>
+              Secure a minimum level of investment from a designated venture
+              capital fund, angel investor group, or business incubator.
+            </p>
+          </div>
+          <div className="uk-container-a">
+            <div className="uk-img-cont">
+              <img src={home} alt="prof" />
+            </div>
+            <p>Meet the language requirements.</p>
+          </div>
+          <div className="uk-container-a">
+            <div className="uk-img-cont">
+              <img src={fast} alt="prof" />
+            </div>
+            <p>
+              Bring along enough money to settle yourself and your dependents.
+            </p>
+          </div>
+          <div className="uk-container-a">
+            <div className="uk-img-cont">
+              <img src={lower} alt="prof" />
+            </div>
+            <p>
+              Bring along enough money to settle yourself and your dependents.
+            </p>
+          </div>
+          <div className="uk-container-a">
+            <div className="uk-img-cont">
+              <img src={flexible} alt="prof" />
+            </div>
+            <p>
+              Bring along enough money to settle yourself and your dependents.
+            </p>
+          </div>
+        </div>
+      </div>
+      <CTA htag="Need Immigration & Visa consultation " red=" For UK" />
 
-        <div
-          className="uk__section-help section__padding "
-          style={{
-            backgroundImage: `url(${back})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-        >
-          <div className="uk__section-logo-container">
+      <div className="uk__section-ability  ">
+        <div className="uk__section-ability-content section__padding">
+          <Fade top>
+            <h1 style={{ color: "#060056" }}>UK's two new Investor programs</h1>
+          </Fade>
+        </div>
+        <div className="uk__section-help section__padding ">
+          <div className="uk__section-logo-container-two">
             <div className="uk-container-a">
-              <div className="img-cont">
-                <img src={biz} alt="prof" />
+              <div className="uk-img-cont">
+                <img src={startup} alt="prof" />
               </div>
               <p>
                 Have a business that qualifies specified criteria by the
@@ -56,57 +126,41 @@ const UK = () => (
               </p>
             </div>
             <div className="uk-container-a">
-              <div className="img-cont">
-                <img src={min} alt="prof" />
+              <div className="uk-img-cont">
+                <img src={innovate} alt="prof" />
               </div>
               <p>
                 Secure a minimum level of investment from a designated venture
                 capital fund, angel investor group, or business incubator.
               </p>
             </div>
-            <div className="uk-container-a">
-              <div className="img-cont">
-                <img src={lang} alt="prof" />
-              </div>
-              <p>Meet the language requirements.</p>
-            </div>
-            <div className="uk-container-a">
-              <div className="img-cont">
-                <img src={money} alt="prof" />
-              </div>
-              <p>
-                Bring along enough money to settle yourself and your dependents.
-              </p>
-            </div>
           </div>
         </div>
-
-        <div className="uk-container section__padding ">
-          <div
-            className="uk__section-helpp section__padding"
-            style={{
-              backgroundImage: `url(${right}) `,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-              backgroundPosition: "right bottom",
-            }}
-          >
-            <div className="uk__section-introo-content">
-              <Fade bottom>
-                <h1 className="gradient__text">How Can Fanuun Help?</h1>
-                <p>
-                  Wondering whether you're eligible for the Canada Start-Up
-                  Visa? <br /> We offer the best consultancy services to our
-                  clients for Canada immigration.
-                </p>
-                <button>Reach out to Fannun Today</button>
-              </Fade>
-            </div>
-          </div>
+      </div>
+      <div
+        className="uk__section-helpp section__margin"
+        style={{
+          backgroundImage: `url(${right}) `,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          backgroundPosition: "right bottom",
+        }}
+      >
+        <div className="uk__section-introo-content ">
+          <Fade bottom>
+            <h1 className="gradient__text">How Can Fanuun Help?</h1>
+            <p>
+              These visas are intended for experienced business people who have
+              a genuine and credible business idea and wish to establish a new
+              business in the UK. This visa program requires the applicant to
+              have an endorsement from an approved endorsing body, such as a
+              business organization or higher education institution that
+              assesses their business idea and confirms that it is innovative,
+              viable and scalable.
+            </p>
+          </Fade>
         </div>
-
-        <WhyFanuun />
-      </div> */}
+      </div>
     </div>
     {/* <Brand /> */}
 
